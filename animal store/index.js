e8097429c97462050895d8fa8a53ed345c237f27
Alex Samih-zade <94469848+maxiboom1@users.noreply.github.com>
2022-10-11 23:23:03 +0300

    Update index.js

--- animal store/index.js
+++ animal store/index.js
@@ -1,8 +1,7 @@
 document.querySelector("#contact").addEventListener('submit',onSubmit);
 
 function onSubmit(event){
-    event.preventDefault();
-    
+    event.preventDefault(); 
     const formData = new FormData(event.target);
     for (const [key, value] of formData) {
         console.log(`Key: ${key}, and value is: ${value}`)
