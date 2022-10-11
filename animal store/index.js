b880d68b14ec15d351008648d9b4f32bf838ef47
Alex Samih-zade <galutziogen@gmail.com>
2022-10-11 23:20:43 +0300

    Add form submit handler

---
+++ animal store/index.js
@@ -0,0 +1,10 @@
+document.querySelector("#contact").addEventListener('submit',onSubmit);
+
+function onSubmit(event){
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    for (const [key, value] of formData) {
+        console.log(`Key: ${key}, and value is: ${value}`)
+    }
+}
