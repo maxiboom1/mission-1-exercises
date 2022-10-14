26be4f0df3950b5d86f83d4056fb01bf16d175ef
Alex Samih-zade <galutziogen@gmail.com>
2022-10-14 23:01:28 +0300

    finish animals store exercise

--- animal store/index.js
+++ animal store/index.js
@@ -1,10 +1,43 @@
 document.querySelector("#contact").addEventListener('submit',onSubmit);
 
 function onSubmit(event){
-    event.preventDefault(); 
+    event.preventDefault();
     const formData = new FormData(event.target);
-    for (const [key, value] of formData) {
-        console.log(`Key: ${key}, and value is: ${value}`)
+    const formObj = formDataBuider(formData);
+    resetBG(document.querySelectorAll("form input"));
+    validation(formObj);
+}
+
+function resetBG(elements){
+    for(const el of elements){
+        el.style.backgroundColor = "white";
     }
 }
- 
+
+function formDataBuider(formData){
+    const dataObj = {};
+    
+    for (const [key, value] of formData) {
+        dataObj[key] = value;
+    }
+    
+    return dataObj;
+}
+
+function validation(formObj){
+    
+    if(formObj.fname == ""){
+        document.querySelector("#fname").style.backgroundColor = "pink";
+        alert("please fill first name");
+    }
+    else if(formObj.lname == ""){
+        document.querySelector("#lname").style.backgroundColor = "pink";
+        alert("please fill last name");
+    }
+    else if(formObj.msg == ""){
+        document.querySelector("#msg").style.backgroundColor = "pink";
+        alert("please fill message");
+    } else {
+        window.location.replace("save.html"); // All valid, continue to save.html page   
+    }
+}
