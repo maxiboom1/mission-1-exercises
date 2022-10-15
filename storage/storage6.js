a69a7c5b1e86226d65d9d4257f6723b33a59540a
Alex Samih-zade <galutziogen@gmail.com>
2022-10-15 12:47:25 +0300

    exs 6 with color selector

--- storage/storage6.js
+++ storage/storage6.js
@@ -1,6 +1,14 @@
 document.querySelector("#pageColor").addEventListener("change", onSelectChange);
 
+const savedColor = localStorage.getItem("bgColor");
+const body = document.querySelector("body");
+
+if (savedColor.length != 0){
+    body.style.backgroundColor = savedColor; // Set saved color onstart
+    document.querySelector("#pageColor").value = savedColor;
+}
+
 function onSelectChange(event){
-    console.log(event.target.value)
-    
+    body.style.backgroundColor = event.target.value; // Set selected color
+    localStorage.setItem("bgColor", event.target.value);
 }
