ff80f62fab0902df972fcc66227db485e3915837
Alex Samih-zade <galutziogen@gmail.com>
2022-10-15 12:55:37 +0300

    finished all 6 storage exercises

--- storage/storage6.js
+++ storage/storage6.js
@@ -1,14 +1,21 @@
 document.querySelector("#pageColor").addEventListener("change", onSelectChange);
 
-const savedColor = localStorage.getItem("bgColor");
 const body = document.querySelector("body");
 
-if (savedColor.length != 0){
-    body.style.backgroundColor = savedColor; // Set saved color onstart
-    document.querySelector("#pageColor").value = savedColor;
-}
+colorInit();
 
 function onSelectChange(event){
     body.style.backgroundColor = event.target.value; // Set selected color
     localStorage.setItem("bgColor", event.target.value);
+}
+
+function colorInit(){
+    
+    let localColor = localStorage.getItem("bgColor");
+    
+    if(localColor.length > 0){
+        body.style.backgroundColor = localColor;
+        document.querySelector("#pageColor").value = localColor; // Set correct value on the color selector
+    }
+
 }
