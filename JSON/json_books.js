1e42f5a304a5bf537f867e076ad2d38057751235
Alex Samih-zade <galutziogen@gmail.com>
2022-10-16 22:30:31 +0300

    Json_books exercise_1 complete

---
+++ JSON/json_books.js
@@ -0,0 +1,42 @@
+const books = [
+    {id:"1", name:"book1_name", autor:"beni_kohen", price:"15"},
+    {id:"2", name:"book2_name", autor:"arik_chen", price:"25"},
+    {id:"3", name:"book3_name", autor:"meir_ariel", price:"32"},
+]
+
+let stringifyed = JSON.stringify(books); // its an book array that stringified with JSON.stringify
+//alert(stringifyed) 
+const items = JSON.parse(stringifyed);
+
+render()
+
+function render(){
+    let content = textContentBuider(books);
+    console.log(content)
+    document.getElementById('dynamic_content').innerHTML = ""; // clear canvas before printing new card_list
+    const node = document.createElement("div");
+    node.innerHTML = content;
+    document.getElementById('dynamic_content').appendChild(node);
+}
+
+function textContentBuider(books){
+    
+    let innerHtmlContent = "";
+    
+    for(let i = 0; i<books.length; i++){
+        
+        innerHtmlContent += createBookText(books[i]);
+
+    } 
+    return innerHtmlContent;
+}
+
+function createBookText(book){
+    const textData = `<p>Book ID: ${book.id}</p>
+    <p>Book Name: ${book.name}</p>
+    <p>Book Autor: ${book.autor}</p>
+    <p>Book Price: ${book.price}</p>
+    <p> - - - - - - - - - - - - - - - - - - - - - - </p>
+    `
+    return textData;
+}
